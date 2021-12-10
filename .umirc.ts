d6140d4fe87728523d83343bbd24e44504604822
linkang.zhu <zhu.zlk@alibaba-inc.com>
2021-12-10 14:51:01 +0800

    feat: add docs

--- .umirc.ts
+++ .umirc.ts
@@ -5,8 +5,14 @@ export default defineConfig({
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  outputPath: 'docs',
+  outputPath: 'docs-dist',
   mode: 'site',
-  antd: {},
+  navs: [
+    null,
+    {
+      title: 'GitHub',
+      path: 'https://github.com/Ankin-IT/mosso-ui',
+    },
+  ],
   // more config: https://d.umijs.org/config
 });
